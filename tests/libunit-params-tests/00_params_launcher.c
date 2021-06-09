ee807f0a8145d51f8d2036ede769608d3ac3a841
Jules Rivoire <julesriv@gmail.com>
2021-06-09 12:03:37 +0200

    [tests] use proper version of launch_tests

--- tests/libunit-params-tests/00_params_launcher.c
+++ tests/libunit-params-tests/00_params_launcher.c
@@ -3,15 +3,16 @@
 int		params_launcher(void)
 {
 	t_unit_lst	*testlist;
+	char *title;
 
 	testlist = NULL;
-	printf("Params Tests: ");
+	title = "Params Tests";
 	int first;
 	first = 1;
 	load_test(&testlist, "Should pass", &one_params_test, &first);
 	int second;
 	second = 0;
 	load_test(&testlist, "Should fail", &one_params_test, &second);
-	return (launch_tests(&testlist));
+	return (launch_tests(&testlist, title));
 }
 
