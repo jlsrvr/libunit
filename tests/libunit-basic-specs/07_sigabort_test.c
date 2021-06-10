fde89d7eda90ca1cdbc12a4c608aaf193bfbec35
Jules Rivoire <julesriv@gmail.com>
2021-06-10 18:58:53 +0200

    [tests] redirect output in basic sigabort test

--- tests/libunit-basic-specs/07_sigabort_test.c
+++ tests/libunit-basic-specs/07_sigabort_test.c
@@ -3,8 +3,12 @@
 
 int sigabort_test(void *params)
 {
+	t_fd *temp;
+
 	(void)params;
 	char* foo = NULL;
+	temp = redirect_stderr("outputs/stderr.log");
 	assert( foo != NULL );
+	reverse_redirect(temp);
 	return (0);
 }
